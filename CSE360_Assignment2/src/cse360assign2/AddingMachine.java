5d7977b998c3d5304f68fb5fd5ab92eb184bd0be
tyler <tyler@10.10.10.108>
2019-10-07 19:16:21 -0700

    Initial version.

---
+++ CSE360_Assignment2/src/cse360assign2/AddingMachine.java
@@ -0,0 +1,35 @@
+/* Author: Tyler Neerings, 1209589391
+ * Class ID: CSE 360, 70641
+ * Assignment: 2
+ * AddingMachine class: This class performs adding function that are yet to be implemented.
+ */ 
+package cse360assign2;
+
+public class AddingMachine {
+
+	private int total;
+	
+	public AddingMachine () {
+		total = 0;  // not needed - included for clarity
+	}
+	
+	public int getTotal () {
+		return 0;
+	}
+	
+	public void add (int value) {
+		
+	}
+	
+	public void subtract (int value) {
+		
+	}
+		
+	public String toString () {
+		return "";
+	}
+
+	public void clear() {
+	
+	}
+}
