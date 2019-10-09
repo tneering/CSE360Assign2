2dfd05d4a6162999cfe59cf9f68de7549f46e4cc
tyler <tyler@ZYTAN.3521.dhcp.asu.edu>
2019-10-09 14:09:19 -0700

    Final version.
    Edited the external text at the top to include the methods and what they
    accomplish.

--- CSE360_Assignment2/src/cse360assign2/AddingMachine.java
+++ CSE360_Assignment2/src/cse360assign2/AddingMachine.java
@@ -1,8 +1,12 @@
 /* Author: Tyler Neerings, 1209589391
  * Class ID: CSE 360, 70641
  * Assignment: 2
- * AddingMachine class: This class will add and subtract values from a private int total, 
- *                      and will log the changes in the private String history.
+ * AddingMachine class: This class will add and subtract values from a private int total, and will log the changes 
+ *                      in the private String history. 
+ *                      Contains methods: getTotal() to return the int total; add(int) to add that int to the total, 
+ *                      and log it in string history; subtract(int) to subtract that int from the total, and log it 
+ *                      in string history; toString() to return the string of the history of additions and subtractions; 
+ *                      clear() to set total and history to default values 0.
  */ 
 
 package cse360assign2;
